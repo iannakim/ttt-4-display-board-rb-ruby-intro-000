907ea58b4c94e635f83a6c7ead34c3bb60632452
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-11 22:12:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,6 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board[4] = X
+
 def display_board(board)
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
+ puts "#{board[0]}|#{board[1]}|#{board[2]}   "
 end
